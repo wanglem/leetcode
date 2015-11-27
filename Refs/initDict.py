e6470c9759fcc83b4e79e34617f658c5f3517552
Zeyang Wang <wanglem34@gmail.com>
2015-11-26 21:55:33 -0500

    reference folder for python cheetsheet

---
+++ Refs/initDict.py
@@ -0,0 +1,8 @@
+
+# create a dictionary of {"one": 1, "two": 2, "three": 3}:
+a = dict(one=1, two=2, three=3)
+b = {'one': 1, 'two': 2, 'three': 3}
+c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+d = dict([('two', 2), ('one', 1), ('three', 3)])
+e = dict({'three': 3, 'one': 1, 'two': 2})
+a == b == c == d == e
