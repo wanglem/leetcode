ff853740d2c9de55ce61697db6eac172e61ab280
Zeyang Wang <wanglem34@gmail.com>
2015-12-02 21:03:20 -0500

    merge k sorted lists

--- solution/mergeKSortedList.py
+++ solution/mergeKSortedList.py
@@ -10,4 +10,24 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        
+        if len(lists) == 0: return []
+        if len(lists) == 1:return lists[0]
+        if len(lists) == 2: return self.mergeTwoLists(lists[0], lists[1])
+        mid = len(lists)/2
+        return self.mergeTwoLists(self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:]))
+
+    def mergeTwoLists(self, l, r):
+        head = ListNode(0)
+        run = head
+        while l is not None and r is not None:
+            if l.val > r.val:
+                run.next = r
+                r = r.next
+            else:
+                run.next = l
+                l = l.next
+            run = run.next
+        if l is None: l = r
+        run.next = l
+
+        return head.next
