591ba97fd804183fb48dbb3d76e2c0576b09a440
Zeyang Wang <wanglem34@gmail.com>
2016-02-04 00:54:22 -0500

    99. recover bst, tricky, worth review

---
+++ solution/recoverBST.py
@@ -0,0 +1,18 @@
+# only two nodes swapped
+# solution:
+# dfs bottom to top dicovered first two violating nodes are the ones we want!
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        
