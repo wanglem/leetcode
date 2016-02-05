c86c998f8a0b2d939c8023398140a043fe0845de
Zeyang Wang <wanglem34@gmail.com>
2016-02-05 17:12:22 -0500

    reference on dict operation

--- Refs/initDict.py
+++ Refs/initDict.py
@@ -6,3 +6,10 @@ c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
 d = dict([('two', 2), ('one', 1), ('three', 3)])
 e = dict({'three': 3, 'one': 1, 'two': 2})
 a == b == c == d == e
+
+print a.keys()
+print a.values()
+del a['one'] # delete by key
+
+for i in a: # iterate on key
+	print i
