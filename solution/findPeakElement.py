73b6944cd0f392e743ab09cbaf81d78c137016ac
Zeyang Wang <wanglem34@gmail.com>
2016-02-11 15:30:29 -0500

    162. WTF

---
+++ solution/findPeakElement.py
@@ -0,0 +1,8 @@
+# 162. find one peak element in a array (i greater than it's neighbor)
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        pass
