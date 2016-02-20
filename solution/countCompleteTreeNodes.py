2c618bd6deff99bccd8a23e3cc391ce4b18e61c2
Zeyang Wang <wanglem34@gmail.com>
2016-02-20 01:40:12 -0500

    222. count nodes on complete tree

---
+++ solution/countCompleteTreeNodes.py
@@ -0,0 +1,17 @@
+# 222. count nodes of complete tree
+# binary search where's right most leaf has the deepest depth
+# more about impl
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def countNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
