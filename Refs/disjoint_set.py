52174bcf8f91c36847dbfec4ef251b3cf0f29ead
Zeyang Wang <wanglem34@gmail.com>
2016-03-01 17:48:51 -0500

    more notes on union find

--- Refs/disjoint_set.py
+++ Refs/disjoint_set.py
@@ -23,3 +23,17 @@
 # Done
 
 # each representative contains a friends group.
+
+
+# two ways of union find here:
+# 1. dedicated leader of each group, leader will keep a counter of number of nodes it has, and all
+#    others point to leader when a new position is added, add it to the largest group, and update 
+#    other adjacnet groups to the largest group
+# pro: find leader quickly, check if two nodes in same group in O(1)
+# con: add node op can be expensive because require update each node to point to new leader
+# 
+# 2. each node only point to its parent, (leader points to itself). If a position added, it becomes 
+#    a new leader. For each neighbor, find the leader of that group and point it to this new leader,
+#    also update new leader's group count.
+# pro: add node operation is cheaper
+# con: find leader is slower, check two nodes same group is slower
