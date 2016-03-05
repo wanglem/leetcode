37839cd34470ed66cf0e2c0fd10c89600e101e00
Zeyang Wang <wanglem34@gmail.com>
2016-03-05 00:57:44 -0500

    union find notes, good impl, parent re-assign and rank to balance

--- Refs/practice.py
+++ Refs/practice.py
@@ -34,4 +34,33 @@ def bsearch(nums, target):
 		else: hi = mid
 	return False
 
-print bsearch([1,3,4,5,7,8,9], 9)
+
+def unionFind(n, edges):
+	def find(x):
+		if x != parent[x]:
+			# find parent, and assign parent's parent to current
+			# so next run can go directly to parent!
+			parent[x] = find(parent[x])
+		return parent[x]
+
+	def union(edge):
+		p1, p2 = map(find, edge)
+		# rank[i] shows number of child node i has
+		# used for balance the graph path
+		if rank[p1] <= rank[p2]:
+			parent[p2] = p1
+			rank[p1] = rank[p2]+1
+		else:
+			parent[p1] = p2
+			rank[p2] = rank[p1]+1
+
+	parent = range(n)
+	rank = [0]*n
+	map(union, edges)
+	return len({find(x) for x in xrange(n)})
+
+e = [[0, 1], [1, 2], [3, 4]]
+print unionFind(5, e)
+
+
+
