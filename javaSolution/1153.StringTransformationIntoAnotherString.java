4d2ce14d0a4a79f2ca72ed3afb00ad35a24d476b
Zeyang Wang <zeyang.wang@walmart.com>
2020-02-19 19:04:47 -0500

    1153

---
+++ javaSolution/1153.StringTransformationIntoAnotherString.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class StringTransformationIntoAnotherString {
+    public boolean canConvert(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+        if (str1.equals(str2)) return true;
+
+        Map<Character, Character> converted = new HashMap<>();
+        Set<Character> seen = new HashSet<>();
+
+        for (int i = 0; i< str1.length(); i++) {
+            char from = str1.charAt(i);
+            char to = str2.charAt(i);
+            if (converted.containsKey(from) && converted.get(from) != to){
+                return false;
+            }
+            converted.put(from, to);
+            seen.add(to);
+        }
+
+        return seen.size() != 26;
+    }
+
+}
--- javaSolution/Main.java
+++ javaSolution/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int[][] a = {{1,1,1,1}, {1,0,0,0}, {1,0,0,0}, {1,0,0,0}};
-        p(new MaxSideLenSquareWithSumLEThreshold().maxSideLength(a, 6));
+        int[] a = {1,2,3,4,5,6,7,8,9};
+        p(new StringTransformationIntoAnotherString().canConvert("leet", "code"));
 
     }
     private static void aa(int a) {
