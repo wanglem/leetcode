a8bf8c4425721471d5b9cbf57fc2864e82a05da6
Zack Wang <wanglem34@gmail.com>
2020-02-20 20:43:01 -0500

    946

--- javaSolution/946.ValidateStackSequences.java
+++ javaSolution/946.ValidateStackSequences.java
@@ -1,25 +1,24 @@
+import java.util.Stack;
+
 class ValidateStackSequences {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         if (pushed.length == 0 && popped.length == 0) return true;
         if (pushed.length != popped.length) return false;
-        int firstPoppedIdx = 0;
-        for (int i = 0; i < pushed.length; i++) {
-            if (pushed[i] == popped[0]) {
-                firstPoppedIdx = i;
-                break;
-            }
-        }
-        int right = pushed.length-1;
-        int left = firstPoppedIdx;
-        for (int i = 0; i < popped.length; i++) {
-            if (left >= 0 && popped[i] == pushed[left]) {
-                left--;
-            } else if (right > firstPoppedIdx && popped[i] == pushed[right]) {
-                right--;
+
+        Stack<Integer> buf = new Stack<>();
+        int m = pushed.length;
+        int i = 0;
+        int j = 0;
+        while (i <= m && j < m) {
+            if (!buf.isEmpty() && buf.peek() == popped[j]) {
+                buf.pop();
+                j++;
             } else {
-                return false;
+                if (i == m) return false;
+                buf.push(pushed[i++]);
             }
         }
-        return true;
+
+        return i == m && j == m;
     }
 }
