a706f0c7a6e88b7c65734dc74ca9be89b2e42f3f
Zack Wang <wanglem34@gmail.com>
2020-02-22 02:15:18 -0500

    some comment

--- javaSolution/299.BullsAndCows.java
+++ javaSolution/299.BullsAndCows.java
@@ -1,6 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
-
+/*
+    one pass:
+        use one array to track.
+        If equals bulls++;
+        for [sc,gc] of each secret/guess char pair:
+            ++ on sc (positive num means previous we saw sc)
+            -- on gc (negative num means previous we saw gc)
+            if S[sc] < 0, means previously had "this sc char" in guess -> cows++
+            if S[gc] > 0 means previously had "this gc char" in secret -> cows++
+ */
 class BullsAndCows {
     public String getHint(String secret, String guess) {
         int[] S = new int[10];
