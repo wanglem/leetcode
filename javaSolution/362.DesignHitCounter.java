802bb29b1ba85e274c6840a194095303213d52b3
Zeyang Wang <zeyang.wang@walmart.com>
2020-03-01 20:52:30 -0500

    362

---
+++ javaSolution/362.DesignHitCounter.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+
+class DesignHitCounter {
+    private class SecUnit {
+        int timestamp;
+        int count;
+        SecUnit(int timestamp) {
+            this.timestamp = timestamp;
+            count = 1;
+        }
+    }
+
+    private LinkedList<SecUnit> window;
+    private int count;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        window = new LinkedList<>();
+        count = 0;
+    }
+
+    /** Record a hit.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+//        enforceWindow(timestamp);
+        if (window.size() > 0 && window.getLast().timestamp == timestamp) {
+            window.getLast().count++;
+        } else {
+            window.add(new SecUnit(timestamp));
+        }
+        count++;
+    }
+
+    /** Return the number of hits in the past 5 minutes.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        enforceWindow(timestamp);
+        return count;
+    }
+
+    private void enforceWindow(int current) {
+        while (window.size() != 0 && current - window.getFirst().timestamp >= 300) {
+            count -= window.removeFirst().count;
+        }
+    }
+}
